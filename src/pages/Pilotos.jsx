3018ee9cff777b74248c02f4bf67adad9fd1883e
Renato <renatocabral2000@gmail.com>
2025-03-11 19:47:55 -0300

    criação de hook customizado

--- src/pages/Pilotos.jsx
+++ src/pages/Pilotos.jsx
@@ -1,31 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from 'react';
+import useFetch from '../hooks/useFetch.js';
 import styles from './Pilotos.module.css';
 
 const Pilotos = () => {
+  const { data: allDrivers, loading, error } = useFetch('https://api.openf1.org/v1/drivers');
   const [drivers, setDrivers] = useState([]);
-  const [allDrivers, setAllDrivers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [loadedCount, setLoadedCount] = useState(10);
 
   useEffect(() => {
-    axios
-      .get('https://api.openf1.org/v1/drivers')
-      .then((response) => {
-        setAllDrivers(response.data);
-        setDrivers(response.data.slice(0, 10));
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err);
-        setLoading(false);
-      });
-  }, []);
+    if (allDrivers) {
+      setDrivers(allDrivers.slice(0, loadedCount));
+    }
+  }, [allDrivers, loadedCount]);
 
   const loadMoreDrivers = () => {
     const newCount = loadedCount + 10;
-    if (newCount <= 60) { 
+    if (newCount <= 60) {
       setLoadedCount(newCount);
       setDrivers(allDrivers.slice(0, newCount));
     }
@@ -40,24 +30,24 @@ const Pilotos = () => {
   }
 
   return (
-    <section>
-      <div className={styles.container}>
-        <h1>Lista de Pilotos:</h1>
-        <div className={styles.grid}>
-          {drivers.map((driver, index) => (
-            <div key={`${driver.driver_number}-${index}`} className={styles.driverCard}>
-              <img src={driver.headshot_url} alt={driver.full_name} />
-              <h2>{driver.full_name}</h2>
-              <p><strong>Time:</strong> {driver.team_name}</p>
-            </div>
-          ))}
-        </div>
+      <section>
+        <div className={styles.container}>
+          <h1>Lista de Pilotos:</h1>
+          <div className={styles.grid}>
+            {drivers.map((driver, index) => (
+                <div key={`${driver.driver_number}-${index}`} className={styles.driverCard}>
+                  <img src={driver.headshot_url} alt={driver.full_name} />
+                  <h2>{driver.full_name}</h2>
+                  <p><strong>Time:</strong> {driver.team_name}</p>
+                </div>
+            ))}
+          </div>
 
-        {loadedCount < 60 && ( 
-          <button onClick={loadMoreDrivers}>Carregar mais pilotos</button>
-        )}
-      </div>
-    </section>
+          {loadedCount < 60 && (
+              <button onClick={loadMoreDrivers}>Carregar mais pilotos</button>
+          )}
+        </div>
+      </section>
   );
 };
 
